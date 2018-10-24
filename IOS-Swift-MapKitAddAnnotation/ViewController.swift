888e7f9d6afeac90d2f177984a4e2e7032c445fd
amhatami <am.hatami@gmail.com>
2018-10-24 16:36:06 -0700

    done

--- IOS-Swift-MapKitAddAnnotation/ViewController.swift
+++ IOS-Swift-MapKitAddAnnotation/ViewController.swift
@@ -7,14 +7,73 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    var keyLat:String = "49.2768"
+    var keyLon:String = "-123.1120"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        mapView.delegate = self
+        
+        let longPressRecogniser = UILongPressGestureRecognizer(target: self, action: #selector(self.handleTap(_:)))
+        longPressRecogniser.minimumPressDuration = 0.5
+        mapView.addGestureRecognizer(longPressRecogniser)
+        
+        mapView.mapType = MKMapType.standard
+        
+        let location = CLLocationCoordinate2D(latitude: CLLocationDegrees(keyLat.toFloat()),longitude: CLLocationDegrees(keyLon.toFloat()))
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = "BC Place Stadium"
+        annotation.subtitle = "Vancouver Canada"
+        mapView.addAnnotation(annotation)
     }
 
 
+    @objc func handleTap(_ gestureReconizer: UILongPressGestureRecognizer)
+    {
+        
+        let location = gestureReconizer.location(in: mapView)
+        let coordinate = mapView.convert(location,toCoordinateFrom: mapView)
+        
+        // Add annotation:
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        annotation.title = "latitude:" + String(format: "%.02f",annotation.coordinate.latitude) + "& longitude:" + String(format: "%.02f",annotation.coordinate.longitude)
+        mapView.addAnnotation(annotation)
+        
+    }
+    
+    
+    var selectedAnnotation: MKPointAnnotation?
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        
+        let latValStr : String = String(format: "%.02f",Float((view.annotation?.coordinate.latitude)!))
+        let lonvalStr : String = String(format: "%.02f",Float((view.annotation?.coordinate.longitude)!))
+        
+        print("latitude: \(latValStr) & longitude: \(lonvalStr)")
+    }
+    
 }
 
+// Extend String with Two functions for converting Float and Double to string
+extension String {
+    func toDouble() -> Double? {
+        return NumberFormatter().number(from: self)?.doubleValue
+    }
+    func toFloat() -> Float {
+        return (self as NSString).floatValue
+    }
+}
